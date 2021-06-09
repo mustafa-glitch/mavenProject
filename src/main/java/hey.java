384b46a96af58b26932cae66a66af2415a1c60e2
Mustafa Ubeyde BUYUKAY <81827430+mustafa-glitch@users.noreply.github.com>
2021-06-09 23:31:04 +0300

    inital commmit

---
+++ src/main/java/hey.java
@@ -0,0 +1,8 @@
+public class hey {
+    public static void main(String[] args) {
+        System.out.println("hello from hey");
+
+    }
+
+}
+
---
+++ src/test/java/hellotest.java
@@ -0,0 +1,7 @@
+public class hellotest {
+
+    public static void main(String[] args) {
+        System.out.println("helloo");
+
+    }
+}
