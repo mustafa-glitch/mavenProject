01d0795dbe4e67094bce849295d9842b5c96beb7
Mustafa Ubeyde BUYUKAY <81827430+mustafa-glitch@users.noreply.github.com>
2021-06-09 23:31:53 +0300

    sec commit

--- src/main/java/hey.java
+++ src/main/java/hey.java
@@ -2,6 +2,8 @@ public class hey {
     public static void main(String[] args) {
         System.out.println("hello from hey");
 
+        System.out.println("developing more commmit");
+
     }
 
 }
